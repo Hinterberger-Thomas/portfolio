00e0d727606afdf59bc4d0d163ace08eb27bb979
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2021-10-21 22:54:02 +0200

    moved vue project

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Nav></Nav>
+  </div>
+  <router-view />
+</template>
+<script lang="ts">
+import Nav from "./components/nav/Nav.vue";
+
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
+<style src="./style/app.scss" lang="scss">
+</style>
---
+++ frontend/src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    component: import("@/views/Home.vue")
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
