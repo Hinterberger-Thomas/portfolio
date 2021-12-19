195000b74b04126d40d7f33d844707aab102229e
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2021-12-19 01:52:37 +0100

    added font & prefix

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/App.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline w-0">
-    Hello world!
+  <h1>
+    <HelloWorld/>
   </h1>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Oxygen', sans-serif;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import './design/main.css'
 
 createApp(App).mount('#app')
