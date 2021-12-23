f3ab80895b4a44645a9dc6e6b1ecfc6ba95a238f
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2021-12-23 20:50:02 +0100

    init svelte project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import './design/main.css'
+import App from './App.svelte'
 
-createApp(App).mount('#app')
+const app = new App({
+  target: document.getElementById('app')
+})
+
+export default app
