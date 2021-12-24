e30d265ccb04add33ba2f31f860b4749ce4476ae
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2021-12-24 15:08:45 +0100

    init vue project

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import path from 'path'
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
-  plugins: [svelte()]
+  plugins: [vue()]
 })
