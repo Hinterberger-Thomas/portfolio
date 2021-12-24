43f50dfcd4741a460350843b0094358c17043b34
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2021-12-24 15:19:13 +0100

    added prefix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+//@ts-ignore
+import path from 'path'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      //@ts-ignore
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
   plugins: [vue()]
 })
